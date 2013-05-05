145228c18a75c087e8f800cabe51583790ed6244
Antony Bailey <support@antonybailey.net>
2013-05-05 16:25:09 -0600

    Not very trusting today

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y
 gpg --import antonybailey.asc
+gpg --list-keys --with-colons --with-fingerprint `gpg --list-keys \
 --with-colons | grep "pub:f:.*:-:" | sed -r -e \
 's/pub:f:[0-9]+:[0-9]+:([A-F0-9]+):.*/0x\1/'` | grep "^fpr:" \
 | sed -r -e 's/fpr:::::::::([0-9A-F]+):/\1:6:/' | gpg --import-ownertrust
